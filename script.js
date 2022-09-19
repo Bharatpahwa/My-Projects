a9a9c28fa6440a0302e5a6d82664cd30d5fa2eb4
Bharat Pahwa <102665650+Bharatpahwa@users.noreply.github.com>
2022-09-20 00:42:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 const htpguessButton = document.querySelector("#htpguess");
 const htpdiceButton = document.querySelector("#htpdice");
 const hiddenGuess = document.querySelector("#added-1");
+const htcexpensesbutton = document.querySelector("#htcexpenses");
 const hiddendice = document.querySelector("#added-2");
+const hiddenexpenses = document.querySelector("#added-3");
 const downArrrow = document.querySelector("#arrow");
 const close1 = document.querySelector("#closed");
 const close2 = document.querySelector("#close2");
+const close3 = document.querySelector("#close3");
 
 htpguessButton.addEventListener("click", function () {
   hiddenGuess.style.display = "flex";
@@ -25,4 +28,14 @@ close1.addEventListener("click", function () {
 close2.addEventListener("click", function () {
   hiddendice.style.display = "none";
   htpdiceButton.textContent = "How to Play";
+});
+
+htcexpensesbutton.addEventListener("click", function () {
+  hiddenexpenses.style.display = "flex";
+  htcexpensesbutton.textContent = "Scroll Down";
+});
+
+close3.addEventListener("click", function () {
+  hiddenexpenses.style.display = "none";
+  htcexpensesbutton.textContent = "How to calc.";
 });
